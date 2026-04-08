d28950caa8cc42256157720254c5e0b53f57e1d9
franklin <frank.fkn.23@gmail.com>
2026-04-08 19:01:07 -0300

    adicionando cadastro na rota

--- front/book-store/src/app/app.routes.ts
+++ front/book-store/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Vitrine } from './vitrine/vitrine';
 import { Cesta } from './cesta/cesta';
+import { Cadastro } from './cadastro/cadastro';
 
 export const routes: Routes = [
   { path: '', component: Vitrine },
   { path: 'vitrine', component: Vitrine },
   { path: 'cesta', component: Cesta },
+  { path: 'cadastro', component: Cadastro },
 ];
