797e7d1036225f89605b04e909bd71a6b5be70a7
franklin <frank.fkn.23@gmail.com>
2026-04-08 18:13:05 -0300

    tela cesta

--- front/book-store/src/app/app.routes.ts
+++ front/book-store/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Vitrine } from './vitrine/vitrine';
+import { Cesta } from './cesta/cesta';
 
 export const routes: Routes = [
   { path: '', component: Vitrine },
   { path: 'vitrine', component: Vitrine },
+  { path: 'cesta', component: Cesta },
 ];
---
+++ front/book-store/src/app/cesta/cesta.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Book } from '../models/book';
+import { ItemCesta } from '../models/item-cesta';
+
+@Component({
+  selector: 'app-cesta',
+  imports: [CommonModule],
+  templateUrl: './cesta.html',
+  styleUrl: './cesta.css',
+})
+export class Cesta {
+  mensagem: string = '';
+  lista: ItemCesta[] = [];
+  total: number = 0;
+
+  ngOnInit() {
+    let cesta = localStorage.getItem('cesta');
+    if (cesta != null) {
+      this.lista = JSON.parse(cesta);
+      this.calculaTotal();
+    } else {
+      this.mensagem = 'Sua cesta está vazia.';
+    }
+  }
+
+  calculaTotal() {
+    this.total = 0;
+    for (let obj of this.lista) {
+      this.total += obj.valor * obj.quantidade;
+    }
+  }
+}
